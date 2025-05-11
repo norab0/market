60b147478862c2d3c730e2f04daed1267ac633a1
norab0 <boudarbalanora@gmail.com>
2025-05-11 23:41:42 +0200

    methodes rayon

--- rayon.ts
+++ rayon.ts
@@ -1,44 +1,68 @@
 import { Article } from "./article";
 
-class Rayon{
-    private id: number;
-    private name: string;
-    private articles : Article[];
+export class Rayon {
+    //attibuts privés
+  private id: number;
+  private name: string;
+  private articles: Article[];
 
-    constructor( id: number, name: string){
-        this.id = id;
-        this.name = name;
-        this.articles = [];
-    }
-    getId(): number {
-        return this.id;
-    }
-    getName(): string {
-        return this.name;
-    }
-    getArticles() : Article[] {
-        return this.articles;
-    }
-    setName(newName : string): void{
-        this.name = newName;
-    }
-    setArticles(newArticle : Article[]): void{
-        this.articles = newArticle ; 
-    }
-    count() : number {
-        return this.articles.length;
-    }
-    addArticle(article : Article): void {
-        this.articles.push(article);
-    }
-    removeArticleByName(name : string): void {
-        //savoir si name est dans l'array article
-        for(let i = 0; i < this.count(); i++ ){
-            //si name est dans l'array article alors on
-            if (this.articles[i].getName() === name){
-                //supprime la valeur name du array article
-                this.articles.splice(i,1);
-            } 
+  //constructeur
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+    this.articles = [];
+  }
+  //getter
+  public getId(): number {
+    return this.id;
+  }
+  public getName(): string {
+    return this.name;
+  }
+  public getArticles(): Article[] {
+    return this.articles;
+  }
+  //setter
+  public setName(newName: string): void {
+    this.name = newName;
+  }
+  public setArticles(newArticle: Article[]): void {
+    this.articles = newArticle;
+  }
+  //methodes
+  //methode qui donne combien d'éléments on a dans le tableau
+  public count(): number {
+    return this.articles.length;
+  }
+  //methode qui ajoute un article dans le tableau
+  public addArticle(article: Article): void {
+    this.articles.push(article);
+  }
+  //methode qui supprime un article du tableau à partir de son nom
+  public removeArticleByName(name: string): void {
+    //savoir si name est dans l'array article
+    try {
+      if (this.count() != 0) {
+        for (let i = 0; i < this.count(); i++) {
+          //si name est dans l'array article alors on
+          try {
+            if (this.articles[i].getName() === name) {
+              //supprime la valeur name du array article
+              this.articles.splice(i, 1);
+            } else {
+              throw new Error(
+                "Erreur: L'article " + name + " n'a pas été trouvé !"
+              );
+            }
+          } catch (e) {
+            console.error(e.message);
+          }
         }
+      } else {
+        throw new Error("Erreur: Il n'y a aucun article dans le rayon !");
+      }
+    } catch (e) {
+      console.error(e.message);
     }
+  }
 }
